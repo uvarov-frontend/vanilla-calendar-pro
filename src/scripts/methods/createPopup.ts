f4c265fb2b8437860b249189030b252b980620de
Yury Uvarov <uvarov.frontend@gmail.com>
2022-11-16 22:08:38 +0300

    refactoring createPopup

--- src/scripts/methods/createPopup.ts
+++ src/scripts/methods/createPopup.ts
@@ -3,24 +3,15 @@ import { FormatDateString, IVanillaCalendar } from 'src/types';
 const createPopup = (self: IVanillaCalendar, daysEl: HTMLElement) => {
 	if (!self.popups) return;
 
-	// eslint-disable-next-line no-restricted-syntax
-	for (const day in self.popups) {
-		if (Object.hasOwnProperty.call(self.popups, day)) {
-			const dayBtnEl = daysEl.querySelector(`[data-calendar-day="${day}"]`);
+	Object.keys(self.popups).forEach((date: string) => {
+		const dayBtnEl = daysEl.querySelector(`[data-calendar-day="${date}"]`);
 
-			if (dayBtnEl) {
-				const dayInfo = self.popups[day as FormatDateString];
-				if (dayInfo) {
-					if (dayInfo.modifier) {
-						dayInfo.modifier.split(' ').forEach((cl) => {
-							dayBtnEl.classList.add(cl);
-						});
-					}
-					(dayBtnEl.parentNode as HTMLElement).innerHTML += `<div class="${self.CSSClasses.dayPopup}">${dayInfo.html}</div>`;
-				}
-			}
+		if (dayBtnEl) {
+			const dayInfo = self.popups?.[date as FormatDateString];
+			if (dayInfo?.modifier) dayInfo.modifier.trim().split(' ').forEach((cl) => { dayBtnEl.classList.add(cl); });
+			if (dayInfo?.html) (dayBtnEl.parentNode as HTMLElement).innerHTML += `<div class="${self.CSSClasses.dayPopup}">${dayInfo.html}</div>`;
 		}
-	}
+	});
 };
 
 export default createPopup;
