2b58b9ee9f08f64ff4c016e547786b47fc95e177
Yury Uvarov <uvarov.frontend@gmail.com>
2023-03-31 20:01:32 +0300

    add docs and examples for new site

---
+++ examples/action-handlers-click-on-the-week-number.js
@@ -0,0 +1,22 @@
+import VanillaCalendar from '@uvarov.frontend/vanilla-calendar';
+import '@uvarov.frontend/vanilla-calendar/build/vanilla-calendar.min.css';
+import '@uvarov.frontend/vanilla-calendar/build/themes/light.min.css';
+import '@uvarov.frontend/vanilla-calendar/build/themes/dark.min.css';
+
+const options = {
+  settings: {
+    visibility: {
+      weekNumbers: true,
+    },
+  },
+  actions: {
+    clickWeekNumber(event, number, days, year) {
+      console.log(`Week number: ${number}`);
+      console.log(`Year of the week: ${year}`);
+      console.log('Days of this week:', days);
+    },
+  },
+}
+
+const calendar = new VanillaCalendar('#calendar', options);
+calendar.init();
