7a83428da92c13c28927e379fce82e526c4859df
Yury Uvarov <uvarov.frontend@gmail.com>
2023-04-08 12:54:55 +0300

    add additional-features-date-picker-in-input.js

---
+++ examples/additional-features-date-picker-in-input.js
@@ -0,0 +1,10 @@
+import VanillaCalendar from '@uvarov.frontend/vanilla-calendar';
+import '@uvarov.frontend/vanilla-calendar/build/vanilla-calendar.min.css';
+import '@uvarov.frontend/vanilla-calendar/build/themes/light.min.css';
+import '@uvarov.frontend/vanilla-calendar/build/themes/dark.min.css';
+
+const options = {
+};
+
+const calendar = new VanillaCalendar('#calendar', options);
+calendar.init();
