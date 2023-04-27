f934348b43e2b58a524c0ae8b41d0d5f598c8d16
Yury Uvarov <uvarov.frontend@gmail.com>
2023-04-27 15:00:08 +0300

    fix index.ts

--- demo/index.ts
+++ demo/index.ts
@@ -3,8 +3,32 @@ import '../package/src/styles/vanilla-calendar.css';
 import '../package/src/styles/themes/light.css';
 import '../package/src/styles/themes/dark.css';
 import VanillaCalendar from '../package/src/scripts/main';
+import { Options } from '../package/src';
+
+const config: Options = {
+	settings: {
+		range: {
+			disablePast: true,
+		},
+		selection: {
+			day: 'multiple-ranged',
+			time: true,
+		},
+		visibility: {
+			weekNumbers: true,
+			daysOutside: false,
+		},
+	},
+	actions: {
+		clickDay(event, dates) {
+			console.log(event, dates);
+		},
+	},
+};
 
 document.addEventListener('DOMContentLoaded', () => {
-	const calendar = new VanillaCalendar('#vanilla-calendar');
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore: VanillaCalendar
+	const calendar = new VanillaCalendar('#vanilla-calendar', config as Options);
 	calendar.init();
 });
