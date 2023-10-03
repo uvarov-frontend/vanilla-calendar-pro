12e393c693df2fecd52d30f2d4e49fae7938363c
Yury Uvarov <uvarov.frontend@gmail.com>
2023-10-03 11:15:14 +0300

    fix search selector input

--- package/src/scripts/methods/handlerInput.ts
+++ package/src/scripts/methods/handlerInput.ts
@@ -4,8 +4,8 @@ let currentSelf: null | IVanillaCalendar = null;
 
 const documentClickEvent = (e: MouseEvent) => {
 	if (!currentSelf) return;
-	if ((e.target as HTMLElement).closest(`.${currentSelf.CSSClasses.calendar}`)) return;
-	document.querySelectorAll(`.${currentSelf.CSSClasses.calendar}`).forEach((calendar) => {
+	if ((e.target as HTMLElement).closest(`.${currentSelf.CSSClasses.calendar}.${currentSelf.CSSClasses.calendarToInput}`)) return;
+	document.querySelectorAll(`.${currentSelf.CSSClasses.calendar}.${currentSelf.CSSClasses.calendarToInput}`).forEach((calendar) => {
 		calendar.classList.add((currentSelf as IVanillaCalendar).CSSClasses.calendarHidden);
 	});
 	document.removeEventListener('click', documentClickEvent, { capture: true });
