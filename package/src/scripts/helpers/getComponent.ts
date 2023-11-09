13b44c82b1f95ce327fbd061066766711f6822cb
Yury Uvarov <uvarov.frontend@gmail.com>
2023-11-09 15:54:07 +0300

    refactoring "getComponent"

--- package/src/scripts/helpers/getComponent.ts
+++ package/src/scripts/helpers/getComponent.ts
@@ -1,54 +1,5 @@
-import {
-	ArrowNext,
-	ArrowPrev,
-	Month,
-	Year,
-	Week,
-	Days,
-	Months,
-	Years,
-	WeekNumbers,
-	ControlTime,
-} from './createComponents';
+import * as components from './createComponents';
 
-const getComponent = (pattern: string) => {
-	let FC = null;
-
-	switch (pattern) {
-		case 'ArrowPrev':
-			FC = ArrowPrev;
-			break;
-		case 'ArrowNext':
-			FC = ArrowNext;
-			break;
-		case 'Month':
-			FC = Month;
-			break;
-		case 'Year':
-			FC = Year;
-			break;
-		case 'Week':
-			FC = Week;
-			break;
-		case 'Days':
-			FC = Days;
-			break;
-		case 'Months':
-			FC = Months;
-			break;
-		case 'Years':
-			FC = Years;
-			break;
-		case 'WeekNumbers':
-			FC = WeekNumbers;
-			break;
-		case 'ControlTime':
-			FC = ControlTime;
-			break;
-
-		// no default
-	}
-	return FC;
-};
+const getComponent = (pattern: string) => components[pattern as keyof typeof components];
 
 export default getComponent;
