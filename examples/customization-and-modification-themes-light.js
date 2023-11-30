af6e183b0e2da9d94e0d7fd5ea5e2c22b409da79
Yury Uvarov <uvarov.frontend@gmail.com>
2023-11-30 15:46:39 +0300

    update docs

--- examples/customization-and-modification-themes-light.js
+++ examples/customization-and-modification-themes-light.js
@@ -1,6 +1,5 @@
 import VanillaCalendar from '@uvarov.frontend/vanilla-calendar';
 import '@uvarov.frontend/vanilla-calendar/build/vanilla-calendar.min.css';
-import '@uvarov.frontend/vanilla-calendar/build/themes/light.min.css';
 
 const options = {
   settings: {
--- examples/type-of-calendar-year.js
+++ examples/type-of-calendar-year.js
@@ -1,7 +1,5 @@
 import VanillaCalendar from '@uvarov.frontend/vanilla-calendar';
 import '@uvarov.frontend/vanilla-calendar/build/vanilla-calendar.min.css';
-import '@uvarov.frontend/vanilla-calendar/build/themes/light.min.css';
-import '@uvarov.frontend/vanilla-calendar/build/themes/dark.min.css';
 
 const options = {
   type: 'year',
