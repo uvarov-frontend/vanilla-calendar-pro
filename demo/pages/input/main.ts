0a45d17cfde92fc37675e38b774254974f6f4b5d
Yury Uvarov <uvarov.frontend@gmail.com>
2023-12-03 18:58:55 +0300

    fix demo ts

--- demo/pages/input/main.ts
+++ demo/pages/input/main.ts
@@ -1,16 +1,10 @@
 // import { IOptions } from '@package/types';
 // import VanillaCalendar from '@/package/build/vanilla-calendar.min';
-
 // import '@/package/build/vanilla-calendar.min.css';
-// import '@/package/build/themes/light.min.css';
-// import '@/package/build/themes/dark.min.css';
 
 import { IOptions } from '@package/types';
 import VanillaCalendar from '@src/vanilla-calendar';
-
 import '@src/styles/vanilla-calendar.css';
-import '@src/styles/themes/light.css';
-import '@src/styles/themes/dark.css';
 
 const configInput: IOptions = {
 	input: true,
--- demo/pages/multiple/main.ts
+++ demo/pages/multiple/main.ts
@@ -1,23 +1,16 @@
 // import { IOptions } from '@package/types';
 // import VanillaCalendar from '@/package/build/vanilla-calendar.min';
-
 // import '@/package/build/vanilla-calendar.min.css';
-// import '@/package/build/themes/light.min.css';
-// import '@/package/build/themes/dark.min.css';
 
 import { IOptions } from '@package/types';
 import VanillaCalendar from '@src/vanilla-calendar';
-
 import '@src/styles/vanilla-calendar.css';
-import '@src/styles/themes/light.css';
-import '@src/styles/themes/dark.css';
 
 const config: IOptions = {
 	type: 'multiple',
 	settings: {
 		selection: {
 			day: 'multiple-ranged',
-			cancelableDay: false,
 		},
 		selected: {
 			month: 3,
