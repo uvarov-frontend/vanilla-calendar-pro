608ffda3ae89ddcf00186ca0a568fa5fe0944197
Yury Uvarov <uvarov.frontend@gmail.com>
2024-04-21 10:36:01 +0300

    fix docs

--- examples/date-management-other-today.ts
+++ examples/date-management-other-today.ts
@@ -4,7 +4,7 @@ import 'vanilla-calendar-pro/build/vanilla-calendar.min.css';
 
 const options: IOptions = {
   date: {
-    today: new Date('2022-01-07'),
+    today: new Date('2022-01-07:T00:00:00.000Z'),
   },
 };
 
