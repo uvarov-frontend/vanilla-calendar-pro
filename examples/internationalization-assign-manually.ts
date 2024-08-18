367bbee083500f6b46293588b249c0d0b43c9333
ghiscoding <gbeaulac@gmail.com>
2024-08-18 01:18:03 -0400

    chore: define long week days

--- examples/internationalization-assign-manually.ts
+++ examples/internationalization-assign-manually.ts
@@ -3,13 +3,14 @@ import { IOptions } from 'vanilla-calendar-pro/types';
 import 'vanilla-calendar-pro/build/vanilla-calendar.min.css';
 
 const options: IOptions = {
-  settings: {
-    lang: 'define',
-  },
-  locale: {
-    months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-    weekday: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-  },
+	settings: {
+		lang: 'define',
+	},
+	locale: {
+		months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+		weekday: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+		longWeekday: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+	},
 };
 
 const calendar = new VanillaCalendar('#calendar', options);
