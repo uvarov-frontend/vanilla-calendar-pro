222a210308e662a4dcfc622c54bf514b26d14f14
Yury Uvarov <uvarov.frontend@gmail.com>
2024-10-19 21:52:40 +0300

    update DOMDefault.ts, DOMMonth.ts, DOMMultiple.ts and DOMYear.ts

--- package/src/scripts/templates/DOMDefault.ts
+++ package/src/scripts/templates/DOMDefault.ts
@@ -1,22 +1,22 @@
 import { CSSClasses } from '@package/types';
 
 const DOMDefault = (styles: CSSClasses) => `
-	<div class="${styles.header}">
-		<#ArrowPrev />
-		<div class="${styles.headerContent}">
-			<#Month />
-			<#Year />
-		</div>
-		<#ArrowNext />
-	</div>
-	<div class="${styles.wrapper}">
-		<#WeekNumbers />
-		<div class="${styles.content}">
-			<#Week />
-			<#Days />
-		</div>
-	</div>
-	<#ControlTime />
+  <div class="${styles.header}">
+    <#ArrowPrev />
+    <div class="${styles.headerContent}">
+      <#Month />
+      <#Year />
+    </div>
+    <#ArrowNext />
+  </div>
+  <div class="${styles.wrapper}">
+    <#WeekNumbers />
+    <div class="${styles.content}">
+      <#Week />
+      <#Days />
+    </div>
+  </div>
+  <#ControlTime />
 `;
 
 export default DOMDefault;
--- package/src/scripts/templates/DOMMonth.ts
+++ package/src/scripts/templates/DOMMonth.ts
@@ -1,16 +1,16 @@
 import { CSSClasses } from '@package/types';
 
 const DOMMonths = (styles: CSSClasses) => `
-	<div class="${styles.header}">
-		<div class="${styles.headerContent}">
-			<#Month />
-			<#Year />
-		</div>
-	</div>
-	<div class="${styles.wrapper}">
-		<div class="${styles.content}">
-			<#Months />
-		</div>
-	</div>
+  <div class="${styles.header}">
+    <div class="${styles.headerContent}">
+      <#Month />
+      <#Year />
+    </div>
+  </div>
+  <div class="${styles.wrapper}">
+    <div class="${styles.content}">
+      <#Months />
+    </div>
+  </div>
 `;
 export default DOMMonths;
