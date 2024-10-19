71b17a230a93ddc5c409732ce1b33a606eb4e205
Yury Uvarov <uvarov.frontend@gmail.com>
2024-10-19 22:19:29 +0300

    update create.ts and default.ts

--- package/src/scripts/create.ts
+++ package/src/scripts/create.ts
@@ -1,37 +1,37 @@
-import VanillaCalendar from '@src/vanilla-calendar';
 import getLocale from '@scripts/helpers/getLocale';
-import visibilityArrows from '@scripts/modules/visibilityArrows';
+import changeTheme from '@scripts/modules/changeTheme';
 import createDays from '@scripts/modules/createDays';
 import createDOM from '@scripts/modules/createDOM';
-import visibilityTitle from '@scripts/modules/visibilityTitle';
 import createMonths from '@scripts/modules/createMonths';
 import createTime from '@scripts/modules/createTime';
 import createWeek from '@scripts/modules/createWeek';
 import createYears from '@scripts/modules/createYears';
-import changeTheme from '@scripts/modules/changeTheme';
+import visibilityArrows from '@scripts/modules/visibilityArrows';
+import visibilityTitle from '@scripts/modules/visibilityTitle';
+import type VanillaCalendar from '@src/vanilla-calendar';
 
 const create = (self: VanillaCalendar) => {
-	const types = {
-		default: () => {
-			createWeek(self);
-			createDays(self);
-		},
-		multiple: () => {
-			createWeek(self);
-			createDays(self);
-		},
-		month: () => createMonths(self),
-		year: () => createYears(self),
-	};
+  const types = {
+    default: () => {
+      createWeek(self);
+      createDays(self);
+    },
+    multiple: () => {
+      createWeek(self);
+      createDays(self);
+    },
+    month: () => createMonths(self),
+    year: () => createYears(self),
+  };
 
-	changeTheme(self);
-	getLocale(self);
-	createDOM(self);
-	visibilityTitle(self);
-	visibilityArrows(self);
-	createTime(self);
+  changeTheme(self);
+  getLocale(self);
+  createDOM(self);
+  visibilityTitle(self);
+  visibilityArrows(self);
+  createTime(self);
 
-	types[self.currentType]();
+  types[self.currentType]();
 };
 
 export default create;
