7e06ed4a9b3ed201db339cf0280d9e882e950967
Yury Uvarov <uvarov.frontend@gmail.com>
2024-10-21 15:44:36 +0300

    update visibilityArrows.ts

--- package/src/scripts/modules/visibilityArrows.ts
+++ package/src/scripts/modules/visibilityArrows.ts
@@ -1,64 +1,56 @@
-import VanillaCalendar from '@src/vanilla-calendar';
-import getDateString from '@scripts/helpers/getDateString';
 import getDate from '@scripts/helpers/getDate';
+import getDateString from '@scripts/helpers/getDateString';
+import type VanillaCalendar from '@src/vanilla-calendar';
 
-const setVisibilityArrows = ({
-	arrowPrev, arrowNext, isPrevHidden, isNextHidden,
-}: {
-	arrowPrev: HTMLElement
-	arrowNext: HTMLElement
-	isPrevHidden: boolean | 0
-	isNextHidden: boolean | 0
-}) => {
-	arrowPrev.style.visibility = isPrevHidden ? 'hidden' : '';
-	arrowNext.style.visibility = isNextHidden ? 'hidden' : '';
+const setVisibilityArrows = (arrowPrevEl: HTMLElement, arrowNextEl: HTMLElement, isArrowPrevHidden: boolean, isArrowNextHidden: boolean) => {
+  arrowPrevEl.style.visibility = isArrowPrevHidden ? 'hidden' : '';
+  arrowNextEl.style.visibility = isArrowNextHidden ? 'hidden' : '';
+};
+
+const handleDefaultType = (self: VanillaCalendar, arrowPrevEl: HTMLElement, arrowNextEl: HTMLElement) => {
+  const currentSelectedDate = getDate(getDateString(new Date(self.selectedYear as number, self.selectedMonth as number, 1)));
+  const jumpDateMin = new Date(currentSelectedDate.getTime());
+  const jumpDateMax = new Date(currentSelectedDate.getTime());
+  jumpDateMin.setMonth(jumpDateMin.getMonth() - self.jumpMonths);
+  jumpDateMax.setMonth(jumpDateMax.getMonth() + self.jumpMonths);
+
+  if (!self.settings.selection.year) {
+    self.dateMin.setFullYear(currentSelectedDate.getFullYear());
+    self.dateMax.setFullYear(currentSelectedDate.getFullYear());
+  }
+
+  const isArrowPrevHidden =
+    !self.settings.selection.month ||
+    jumpDateMin.getFullYear() < self.dateMin.getFullYear() ||
+    (jumpDateMin.getFullYear() === self.dateMin.getFullYear() && jumpDateMin.getMonth() < self.dateMin.getMonth());
+  const isArrowNextHidden =
+    !self.settings.selection.month ||
+    jumpDateMax.getFullYear() > self.dateMax.getFullYear() ||
+    (jumpDateMax.getFullYear() === self.dateMax.getFullYear() && jumpDateMax.getMonth() > self.dateMax.getMonth());
+
+  setVisibilityArrows(arrowPrevEl, arrowNextEl, isArrowPrevHidden, isArrowNextHidden);
+};
+
+const handleYearType = (self: VanillaCalendar, arrowPrevEl: HTMLElement, arrowNextEl: HTMLElement) => {
+  const isArrowPrevHidden = !!(self.dateMin.getFullYear() && self.viewYear - 7 <= self.dateMin.getFullYear());
+  const isArrowNextHidden = !!(self.dateMax.getFullYear() && self.viewYear + 7 >= self.dateMax.getFullYear());
+
+  setVisibilityArrows(arrowPrevEl, arrowNextEl, isArrowPrevHidden, isArrowNextHidden);
 };
 
 const visibilityArrows = (self: VanillaCalendar) => {
-	if (self.currentType === 'month') return;
+  if (self.currentType === 'month') return;
 
-	const arrowPrev: HTMLElement | null = self.HTMLElement?.querySelector(`.${self.CSSClasses.arrowPrev}`);
-	const arrowNext: HTMLElement | null = self.HTMLElement?.querySelector(`.${self.CSSClasses.arrowNext}`);
+  const arrowPrevEl = self.HTMLElement.querySelector<HTMLElement>('[data-vc="arrow-prev"]');
+  const arrowNextEl = self.HTMLElement.querySelector<HTMLElement>('[data-vc="arrow-next"]');
+  if (!arrowPrevEl || !arrowNextEl) return;
 
-	if (!arrowPrev || !arrowNext) return;
+  const updateType = {
+    default: () => handleDefaultType(self, arrowPrevEl, arrowNextEl),
+    year: () => handleYearType(self, arrowPrevEl, arrowNextEl),
+  };
 
-	const updateType = {
-		default: () => {
-			const currentSelectedDate = getDate(getDateString(new Date(self.selectedYear as number, self.selectedMonth as number, 1)));
-			const jumpDateMin = new Date(currentSelectedDate.getTime());
-			const jumpDateMax = new Date(currentSelectedDate.getTime());
-			jumpDateMin.setMonth(jumpDateMin.getMonth() - self.jumpMonths);
-			jumpDateMax.setMonth(jumpDateMax.getMonth() + self.jumpMonths);
-
-			if (!self.settings.selection.year) {
-				self.dateMin.setFullYear(currentSelectedDate.getFullYear());
-				self.dateMax.setFullYear(currentSelectedDate.getFullYear());
-			}
-
-			const isPrevHidden = !self.settings.selection.month
-				|| jumpDateMin.getFullYear() < self.dateMin.getFullYear()
-				|| (jumpDateMin.getFullYear() === self.dateMin.getFullYear()
-				&& (jumpDateMin.getMonth() < self.dateMin.getMonth()));
-			const isNextHidden = !self.settings.selection.month
-				|| jumpDateMax.getFullYear() > self.dateMax.getFullYear()
-				|| (jumpDateMax.getFullYear() === self.dateMax.getFullYear()
-				&& jumpDateMax.getMonth() > self.dateMax.getMonth());
-
-			setVisibilityArrows({
-				arrowPrev, arrowNext, isPrevHidden, isNextHidden,
-			});
-		},
-		year: () => {
-			setVisibilityArrows({
-				arrowPrev,
-				arrowNext,
-				isPrevHidden: self.dateMin.getFullYear() && (self.viewYear - 7) <= self.dateMin.getFullYear(),
-				isNextHidden: self.dateMax.getFullYear() && (self.viewYear + 7) >= self.dateMax.getFullYear(),
-			});
-		},
-	};
-
-	updateType[self.currentType === 'multiple' ? 'default' : self.currentType]();
+  updateType[self.currentType === 'multiple' ? 'default' : self.currentType]();
 };
 
 export default visibilityArrows;
