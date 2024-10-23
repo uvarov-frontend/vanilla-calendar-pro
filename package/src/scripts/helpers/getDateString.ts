256f06fd9b64ef2a70b5e6c22d7dc64aec036a7f
Yury Uvarov <uvarov.frontend@gmail.com>
2024-10-23 20:24:11 +0300

    update getDate.ts, getDateString.ts and getLocalDate.ts

--- package/src/scripts/helpers/getDateString.ts
+++ package/src/scripts/helpers/getDateString.ts
@@ -1,6 +1,6 @@
 import type { FormatDateString } from '@package/types';
 
-const getDateString = (date: Date) => {
+const getDateString = (date: Date): FormatDateString => {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
--- package/src/scripts/helpers/getLocalDate.ts
+++ package/src/scripts/helpers/getLocalDate.ts
@@ -1,6 +1,6 @@
 import type { FormatDateString } from '@package/types';
 
-const getLocalDate = () => {
+const getLocalDate = (): FormatDateString => {
   const now = new Date();
   return new Date(now.getTime() - now.getTimezoneOffset() * 60000).toISOString().substring(0, 10) as FormatDateString;
 };
