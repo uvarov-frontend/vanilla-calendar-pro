7a9891b2fbeaa087d55fc09b44289aede5e5b28e
Yury Uvarov <uvarov.frontend@gmail.com>
2024-10-28 14:43:47 +0300

    update handleRange.ts

--- package/src/scripts/handles/handleTime/handleRange.ts
+++ package/src/scripts/handles/handleTime/handleRange.ts
@@ -2,14 +2,15 @@ import setTime from '@scripts/handles/handleTime/setTime';
 import transformTime12 from '@scripts/utils/transformTime12';
 import type VanillaCalendar from '@src/vanilla-calendar';
 
-const updateKeepingTime = (self: VanillaCalendar, keepingTimeEl: HTMLButtonElement | null, keeping: string) => {
-  self.selectedKeeping = keeping;
-  if (keepingTimeEl) keepingTimeEl.innerText = keeping;
-};
-
 const updateInputAndTime = (self: VanillaCalendar, inputEl: HTMLInputElement, value: string, event: Event, type: 'hour' | 'minute') => {
   inputEl.value = value;
   setTime(self, event, value, type);
+};
+
+const updateKeepingTime = (self: VanillaCalendar, keepingTimeEl: HTMLButtonElement | null, keeping: string) => {
+  if (!keepingTimeEl) return;
+  self.selectedKeeping = keeping;
+  keepingTimeEl.innerText = keeping;
 };
 
 const handleRange = (
@@ -23,11 +24,11 @@ const handleRange = (
   const handleRangeAction = (event: Event) => {
     const value = Number(rangeEl.value);
     const valueStr = rangeEl.value.padStart(2, '0');
-    const isHourType = type === 'hour' || max === 12;
-    const ifTime = value < max && value > 0;
+    const isNotHourType = type !== 'hour' || max !== 12;
+    const correctTime = value < max && value > 0;
 
-    updateInputAndTime(self, inputEl, isHourType || ifTime ? valueStr : transformTime12(rangeEl.value), event, type);
-    if (isHourType) updateKeepingTime(self, keepingTimeEl, value === 0 ? 'AM' : value < max ? 'AM' : 'PM');
+    updateInputAndTime(self, inputEl, isNotHourType || correctTime ? valueStr : transformTime12(rangeEl.value), event, type);
+    if (!isNotHourType) updateKeepingTime(self, keepingTimeEl, value === 0 || correctTime ? 'AM' : 'PM');
   };
 
   rangeEl.addEventListener('input', handleRangeAction);
