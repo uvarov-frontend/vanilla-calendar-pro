96262bbe5a636cf448f12e457dca1da660844ac5
Yury Uvarov <uvarov.frontend@gmail.com>
2024-10-31 20:52:23 +0300

    update getCorrectNumberOfMonths.ts

--- package/src/scripts/utils/getCorrectNumberOfMonths.ts
+++ package/src/scripts/utils/getCorrectNumberOfMonths.ts
@@ -1,5 +1,6 @@
 import type VanillaCalendar from '@src/vanilla-calendar';
 
-const getCorrectNumberOfMonths = (self: VanillaCalendar) => (self.viewType === 'multiple' ? (self.displayMonthsCount === 1 ? 2 : self.displayMonthsCount > 12 ? 12 : self.displayMonthsCount) : 1);
+const getCorrectNumberOfMonths = (self: VanillaCalendar) =>
+  self.viewType === 'multiple' ? (self.displayMonthsCount === 1 ? 2 : self.displayMonthsCount > 12 ? 12 : self.displayMonthsCount) : 1;
 
 export default getCorrectNumberOfMonths;
