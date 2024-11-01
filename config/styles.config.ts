b6c874d3ec19a3840062722c25f8e786eea8c2de
Yury Uvarov <uvarov.frontend@gmail.com>
2024-11-01 19:00:11 +0300

    fix build

--- config/styles.config.ts
+++ config/styles.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     outDir,
     cssCodeSplit: true,
     minify: false,
-    emptyOutDir: false,
+    emptyOutDir: true,
     rollupOptions: {
       output: {
         inlineDynamicImports: false,
--- package/build/scripts/vanilla-calendar.js
+++ package/build/scripts/vanilla-calendar.js
@@ -1,4 +1,4 @@
-/*! name: vanilla-calendar-pro v3.0.0-beta.2 | url: https://github.com/uvarov-frontend/vanilla-calendar-pro */
+/*! name: vanilla-calendar-pro v3.0.0-beta.3 | url: https://github.com/uvarov-frontend/vanilla-calendar-pro */
 (function(global, factory) {
   typeof exports === "object" && typeof module !== "undefined" ? module.exports = factory() : typeof define === "function" && define.amd ? define(factory) : (global = typeof globalThis !== "undefined" ? globalThis : global || self, global.VanillaCalendar = factory());
 })(this, function() {
