e3c162c8ce210417581ad2daaf90e16219763eb2
Yury Uvarov <uvarov.frontend@gmail.com>
2024-11-06 15:47:56 +0300

    fix docs and examples

--- examples/date-management-selected-days-month-year-by-default.ts
+++ examples/date-management-selected-days-month-year-by-default.ts
@@ -1,6 +1,6 @@
 import { Calendar, type Options } from 'vanilla-calendar-pro';
 
-import 'vanilla-calendar-pro/styles/vanilla-calendar-pro.min.css';
+import 'vanilla-calendar-pro/styles/index.css';
 
 const options: Options = {
   selectionDatesMode: 'multiple',
--- examples/try-vanilla-calendar.ts
+++ examples/try-vanilla-calendar.ts
@@ -1,6 +1,6 @@
 import { Calendar } from 'vanilla-calendar-pro';
 
-import 'vanilla-calendar-pro/styles/vanilla-calendar-pro.min.css';
+import 'vanilla-calendar-pro/styles/index.css';
 
 const calendar = new Calendar('#calendar');
 calendar.init();
