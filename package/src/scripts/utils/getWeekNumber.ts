7638b232c7afdc89c2ce4e9ec1ff4ac960316f79
Yury Uvarov <uvarov.frontend@gmail.com>
2026-08-12 13:34:45 +0300

    correct off-by-one in ISO week number calculation

--- package/src/scripts/utils/getWeekNumber.ts
+++ package/src/scripts/utils/getWeekNumber.ts
@@ -4,7 +4,7 @@ import type { FormatDateString, WeekDayID } from '@src/index';
 const getWeekNumber = (date: FormatDateString, weekStartDay: WeekDayID) => {
   const currentDate = getDate(date);
   const currentDay = (currentDate.getDay() - weekStartDay + 7) % 7;
-  currentDate.setDate(currentDate.getDate() + 4 - currentDay);
+  currentDate.setDate(currentDate.getDate() + 3 - currentDay);
 
   const yearStart = new Date(currentDate.getFullYear(), 0, 1);
   const weekNumber = Math.ceil(((+currentDate - +yearStart) / 86400000 + 1) / 7);
