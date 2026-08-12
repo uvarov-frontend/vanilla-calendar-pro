f55a2d551032bf7f46267d8f4769baef0ba037b2
Yury Uvarov <uvarov.frontend@gmail.com>
2026-08-12 12:21:55 +0300

    reset displayDateMin/displayDateMax via null in .set()

--- package/src/scripts/utils/resolveDate.ts
+++ package/src/scripts/utils/resolveDate.ts
@@ -2,7 +2,7 @@ import getLocalDate from '@scripts/utils/getLocalDate';
 import parseDates from '@scripts/utils/parseDates';
 import type { DateAny, FormatDateString } from '@src/index';
 
-const resolveDate = (date: 'today' | Date | number | string | undefined, defaultDate: DateAny): FormatDateString => {
+const resolveDate = (date: 'today' | Date | number | string | null | undefined, defaultDate: DateAny): FormatDateString => {
   if (date === 'today') return getLocalDate();
   if (date instanceof Date || typeof date === 'number' || typeof date === 'string') return parseDates([date])[0];
   return defaultDate as FormatDateString;
