b9f94dfaf2f0cdb9853cc8055924d2740223dcc0
Yury Uvarov <uvarov.frontend@gmail.com>
2026-08-23 15:14:50 +0300

    docs: add week, collapse and swipe guides and examples

--- examples/additional-features-collapse.ts
+++ examples/additional-features-collapse.ts
@@ -3,12 +3,10 @@ import { Calendar, type Options } from 'vanilla-calendar-pro';
 import 'vanilla-calendar-pro/styles/index.css';
 
 const options: Options = {
-  animation: {
-    slide: { duration: 700, easing: 'cubic-bezier(0.68, -0.55, 0.27, 1.55)' },
-    fade: { duration: 450, easing: 'ease-in-out' },
-    collapse: { duration: 550, easing: 'ease-in-out' },
-  },
+  animation: true,
   enableCollapse: true,
+  selectedDates: ['2024-06-19'],
+  enableJumpToSelectedDate: true,
 };
 
 const calendar = new Calendar('#calendar', options);
--- examples/additional-features-gestures.ts
+++ examples/additional-features-gestures.ts
@@ -3,12 +3,11 @@ import { Calendar, type Options } from 'vanilla-calendar-pro';
 import 'vanilla-calendar-pro/styles/index.css';
 
 const options: Options = {
-  animation: {
-    slide: { duration: 700, easing: 'cubic-bezier(0.68, -0.55, 0.27, 1.55)' },
-    fade: { duration: 450, easing: 'ease-in-out' },
-    collapse: { duration: 550, easing: 'ease-in-out' },
-  },
+  animation: { collapse: { duration: 450 }, slide: { duration: 350 } },
   enableCollapse: true,
+  enableSwipe: true,
+  selectedDates: ['2024-06-19'],
+  enableJumpToSelectedDate: true,
 };
 
 const calendar = new Calendar('#calendar', options);
