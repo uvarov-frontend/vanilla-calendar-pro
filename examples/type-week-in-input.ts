625f758d0605f733fb2d46d6c1bb41a5a29f7cf1
Yury Uvarov <uvarov.frontend@gmail.com>
2026-08-23 16:55:26 +0300

    docs: update gesture options and add week input example

--- examples/type-week-in-input.ts
+++ examples/type-week-in-input.ts
@@ -3,11 +3,18 @@ import { Calendar, type Options } from 'vanilla-calendar-pro';
 import 'vanilla-calendar-pro/styles/index.css';
 
 const options: Options = {
-  animation: { collapse: { duration: 450 }, slide: { duration: 350 } },
+  type: 'week',
+  inputMode: true,
+  positionToInput: 'auto',
+  animation: true,
   enableCollapse: true,
   enableSwipe: true,
   selectedDates: ['2024-06-19'],
   enableJumpToSelectedDate: true,
+  onChangeToInput(self) {
+    if (!self.context.inputElement) return;
+    self.context.inputElement.value = self.context.selectedDates[0] ?? '';
+  },
 };
 
 const calendar = new Calendar('#calendar', options);
